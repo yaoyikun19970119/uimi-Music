719f5470f7281e7a2e98f86384301ebab9a15dfb
kun <971909506@qq.com>
2020-03-22 00:17:58 +0800

    完成项目结构调整&&login组件

---
+++ src/views/login.vue
@@ -0,0 +1,40 @@
+<template>
+   <div class="login">
+       <div class="login-title">UIMI 音乐</div>
+       <img class="login-img" src="../assets/login.jpeg" alt="">
+       
+   </div>
+</template>
+
+
+<script lang="ts">
+import { Component, Vue, Emit, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {
+
+   
+}
+</script>
+
+
+<style lang="less" scoped>
+.login{
+    min-width: 1000px;
+    height: 950px;
+    position: relative;
+    background-color: aqua;
+    .login-img{
+        position: absolute;
+        top: 20%;
+        left: 10%;
+        width: 800px;
+        height: 500px;
+    }
+    .login-title{
+        text-align: center;
+        font-size: 50px;
+        padding-top: 50px;
+    }
+}
+</style>
